cb7921bb6f251a28991a6f6b81a3cedd1b01dff3
TudorTacal <tudortacal@gmail.com>
2017-01-23 11:57:56 +0000

    Record the date every time a transactions is made and saves the transactions for that particular date into a hash

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,18 +1,21 @@
 class Bank
 
-  attr_reader :credit, :withdrawals
+  attr_reader :credit, :withdrawals, :transactions
 
   def initialize
     @credit = []
     @withdrawals = []
+    @transactions = Hash.new{|h,k| h[k] = {credit: 0, debit: 0}}
   end
 
-  def add_credit(amount)
+  def add_credit(amount, date)
     credit << amount
+    @transactions["#{date}"][:credit] += amount
   end
 
-  def withdraw_money(amount)
+  def withdraw_money(amount, date)
     withdrawals << amount
+    @transactions["#{date}"][:debit] += amount
   end
 
   def balance
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,11 +6,11 @@ describe Bank do
   context 'Adding credit to the account' do
 
     it 'it should have a method to add the credit into the account' do
-      expect{bank.add_credit(1000)}.to_not raise_error
+      expect{bank.add_credit(1000, '23/01/2017')}.to_not raise_error
     end
 
     it 'should save the added credit to an array' do
-      bank.add_credit(1000)
+      bank.add_credit(1000, '23/01/2017')
       expect(bank.credit).to eq [1000]
     end
   end
@@ -18,19 +18,19 @@ describe Bank do
   context 'Making withdrawals from the account' do
 
     it 'it should have a method to withdraw money from the account' do
-      expect{bank.withdraw_money(1000)}.to_not raise_error
+      expect{bank.withdraw_money(1000, '23/01/2017')}.to_not raise_error
     end
 
     it 'it should save the withdrawn amount into an array' do
-      bank.withdraw_money(1000)
+      bank.withdraw_money(1000, '23/01/2017')
       expect(bank.withdrawals). to eq [1000]
     end
   end
 
   context 'Calculating the balance' do
     it 'it should substract the total withdrawal amount from the total added credit amount' do
-      bank.add_credit(1000)
-      bank.withdraw_money(1000)
+      bank.add_credit(1000, '23/01/2017')
+      bank.withdraw_money(1000, '23/01/2017')
       expect(bank.balance).to eq 0
     end
   end
@@ -39,8 +39,16 @@ describe Bank do
     it 'it should allow to record the date of the transaction' do
       bank.add_credit(1000, '23/01/2017')
       bank.withdraw_money(500, '23/01/2017')
-      expect(bank.date('23/01/2017')).to eq({credit: 1000, debit: 500})
+      expect(bank.transactions.keys).to include('23/01/2017')
     end
+
+    it 'should dipslay the trasactions for that date' do
+      bank.add_credit(1000, '23/01/2017')
+      bank.withdraw_money(500, '23/01/2017')
+      bank.add_credit(1000, '23/01/2017')
+      expect(bank.transactions['23/01/2017']).to eq({credit: 2000, debit: 500})
+    end
+
   end
 end
 
