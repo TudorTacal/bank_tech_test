0a43ec51ceb161bb5ba917df4a648c3f96ec5ad4
TudorTacal <tudortacal@gmail.com>
2017-01-23 10:01:18 +0000

    Wrote the first test for adding credit

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,5 +3,10 @@ require 'bank'
 describe Bank do
   subject(:bank) {described_class.new()}
 
+  describe 'Adding credit to the account' do
+    it 'it should have a method to add the credit into the account' do
+      bank.add_credit(1000).to_not raise_error
+    end
+  end
 
 end
